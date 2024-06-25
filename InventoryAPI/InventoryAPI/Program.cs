d7b4c1f93b2a4b1a20f9a92e7016098694f24ba8
Saebastion Cole <99359888+sdcole@users.noreply.github.com>
2024-06-24 23:24:36 -0500

    Add Serilog Logging functionality.

--- InventoryAPI/InventoryAPI/Program.cs
+++ InventoryAPI/InventoryAPI/Program.cs
@@ -1,9 +1,54 @@
 using InventoryAPI.Models;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.PostgreSQL;
+using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add Serilog configuration
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .WriteTo.PostgreSQL(
+        connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+        tableName: "logs",
+        needAutoCreateTable: true, // This will auto-create the Logs table if it doesn't exist
+        useCopy: false,
+        columnOptions: new Dictionary<string, ColumnWriterBase>
+        {
+            { "Message", new RenderedMessageColumnWriter() },
+            { "MessageTemplate", new MessageTemplateColumnWriter() },
+            { "Level", new LevelColumnWriter() },
+            { "TimeStamp", new TimestampColumnWriter() },
+            { "Exception", new ExceptionColumnWriter() },
+            { "Properties", new PropertiesColumnWriter() }
+        }
+    )
+    .CreateLogger();
+try
+{
+    // Log a message indicating that the web application has started
+    logger.Error("This is annoying");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error logging startup message: {ex.Message}");
+}
+builder.Host.UseSerilog(logger);
 
+// Add policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "DefaultPolicy",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,7 +65,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseCors("DefaultPolicy");
 
 app.UseAuthorization();
 
